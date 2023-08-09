0eef1c88473fea56b6f73ffaa74f43bf9d8a296f
KarenDevia <deviaskm2000@gmail.com>
2023-08-09 12:18:13 +0200

    primeros pasos

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const displayValorAnterior = document.getElementById('valor-anterior');
+const displayValorActual = document.getElementById('valor-actual');
+const botonesNumeros = document.querySelectorAll('.numero');
+const botonesOperadores = document.querySelectorAll('.operador');
+
+
+const calculadora = new Calculadora();
+
+console.log(calculadora.restar(10,8))
